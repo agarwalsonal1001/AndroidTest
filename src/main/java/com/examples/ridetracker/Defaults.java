8fa12d60f3cdd9d42a061f080fc5e9253a9d993c
agarwalsonal1001 <agarwalsonal1001@gmail.com>
2017-11-05 10:31:00 +0530

    first commit

---
+++ src/main/java/com/examples/ridetracker/Defaults.java
@@ -0,0 +1,11 @@
+package com.examples.ridetracker;
+
+public class Defaults
+{
+  
+public static final String APPLICATION_ID = "D3F97B28-80A5-C6EC-FF97-F692B33A7D00";
+public static final String API_KEY = "4A99CEAC-8B07-D1D6-FFAC-BC4D3A059C00";
+public static final String SERVER_URL = "https://api.backendless.com";
+    
+}
+                                            
